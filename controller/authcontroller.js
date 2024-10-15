d1f2b2bf98da1f543ee2c819cc07c0c51a1a5fce
Sagar KC <sagar.kc.a18@gmail.com>
2024-10-15 00:02:38 -0400

    login authentication is complete

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -1,7 +1,19 @@
 
 const { password } = require("pg/lib/defaults");
+
+const bcrypt = require('bcryptjs');
 const user = require("../db/models/user");
 
+const jwt = require('jsonwebtoken');
+
+
+
+
+const generateToken = (payload) =>{
+    return jwt.sign(payload, process.env.JWT_SECRET_KEY,{
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    }) 
+}
 const signup = async (req,res,next) =>{
     const body = req.body;
     if(!['1','2'].includes (body.userType))
@@ -19,8 +31,17 @@ const signup = async (req,res,next) =>{
             password: body.password,
             confirmPassword: body.confirmPassword
         })
+        // result
+        const result = newUser.toJSON();
 
-    if(!newUser){
+        delete result.password;
+        delete result.deletedAt;
+
+        result.token = generateToken({
+            id: result.id
+        })
+
+    if(!result){
             return 
             res.status(400).json({
                 status: 'failed',
@@ -29,9 +50,32 @@ const signup = async (req,res,next) =>{
     }
     res.status(201).json({
         status: 'success',
-        data: newUser,
+        data: result,
     });
-
 };
+const login = async (req,res,next) => {
+    const { email, password } = req.body;
+    if(!email || !password){
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Either the email or password is empty!!',
+        })
+    }
 
-module.exports= {signup};
+    const result = await(user.findOne({where: {email}}));
+    if(!result || ! await(bcrypt.compare(password,result.password))){
+       return res.status(401).json({
+            status: 'fail',
+            message: 'Incorrect email or password',
+        })
+    }
+    const token = generateToken({
+        id:result.id,
+    })
+    return res.json({
+        status: 'success',
+        token,
+    })
+}   
+
+module.exports= {signup, login};
--- route/authRoute.js
+++ route/authRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const {signup} = require('../controller/authcontroller');
+const {signup,login} = require('../controller/authcontroller');
 
 const router = express.Router();
 
 router.route('/signup').post(signup);
+router.route('/login').post(login);
+
 
 module.exports = router;
