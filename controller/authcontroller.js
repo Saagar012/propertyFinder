0ee094de35178cb334a006c3aad2b8c3cf25a5a0
Sagar KC <sagar.kc.a18@gmail.com>
2024-10-20 22:51:26 -0400

    minor changes in authcontroller

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -7,8 +7,6 @@ const user = require("../db/models/user");
 const jwt = require('jsonwebtoken');
 
 
-
-
 const generateToken = (payload) =>{
     return jwt.sign(payload, process.env.JWT_SECRET_KEY,{
         expiresIn: process.env.JWT_EXPIRES_IN,
@@ -79,3 +77,6 @@ const login = async (req,res,next) => {
 }   
 
 module.exports= {signup, login};
+
+
+
